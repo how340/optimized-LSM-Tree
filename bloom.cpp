69d909968c09587a4e900b48b4eda71c3a69ff72
Hongkai Wang <hongkaiwang@hsph.harvard.edu>
2024-02-16 22:14:15 -0500

    added three hashes for bloom filters

--- bloom.cpp
+++ bloom.cpp
@@ -1,112 +1,104 @@
-#include <math.h> 
-#include <iostream>
-#define ll long long 
-using namespace std; 
+#include "bloom.h"
 
-// hash 1 
-int h1(string s, int arrSize) 
-{ 
-	ll int hash = 0; 
-	for (int i = 0; i < s.size(); i++) 
-	{ 
-		hash = (hash + ((int)s[i])); 
-		hash = hash % arrSize; 
-	} 
-	return hash; 
-} 
+// Selection of hash functions inspired by https://llimllib.github.io/bloomfilter-tutorial/
 
-// hash 2 
-int h2(string s, int arrSize) 
-{ 
-	ll int hash = 1; 
-	for (int i = 0; i < s.size(); i++) 
-	{ 
-		hash = hash + pow(19, i) * s[i]; 
-		hash = hash % arrSize; 
-	} 
-	return hash % arrSize; 
-} 
+// FNV-1 hash. 
+// inspired by http://isthe.com/chongo/tech/comp/fnv/#FNV-1a
+uint64_t BloomFilter::hash_1(KEY k) const {
+    // FNV prime and offset(hash) is dependent on bit length. 
+    const uint32_t Prime = 0x01000193; 
+    const uint32_t hash  = 0x811C9DC5; 
+  
+     for (int i = 0; i < 4; ++i) {
+        uint8_t byte = (k >> (i * 8)) & 0xFF; // Extract byte from the integer
+        hash ^= byte;                            // XOR the bottom with the current byte
+        hash *= FNV_prime;                       // Multiply by the FNV prime
+    }
 
-// hash 3 
-int h3(string s, int arrSize) 
-{ 
-	ll int hash = 7; 
-	for (int i = 0; i < s.size(); i++) 
-	{ 
-		hash = (hash * 31 + s[i]) % arrSize; 
-	} 
-	return hash % arrSize; 
-} 
-
-// hash 4 
-int h4(string s, int arrSize) 
-{ 
-	ll int hash = 3; 
-	int p = 7; 
-	for (int i = 0; i < s.size(); i++) { 
-		hash += hash * 7 + s[0] * pow(p, i); 
-		hash = hash % arrSize; 
-	} 
-	return hash; 
-} 
-
-
-// lookup operation 
-bool lookup(bool* bitarray, int arrSize, string s) 
-{ 
-	int a = h1(s, arrSize); 
-	int b = h2(s, arrSize); 
-	int c = h3(s, arrSize); 
-	int d = h4(s, arrSize); 
-
-	if (bitarray[a] && bitarray[b] && bitarray 
-		&& bitarray[d]) 
-		return true; 
-	else
-		return false; 
-} 
-
-// insert operation 
-void insert(bool* bitarray, int arrSize, string s) 
-{ 
-	// check if the element in already present or not 
-	if (lookup(bitarray, arrSize, s)) 
-		cout << s << " is Probably already present" << endl; 
-	else
-	{ 
-		int a = h1(s, arrSize); 
-		int b = h2(s, arrSize); 
-		int c = h3(s, arrSize); 
-		int d = h4(s, arrSize); 
-
-		bitarray[a] = true; 
-		bitarray[b] = true; 
-		bitarray[c] = true; 
-		bitarray[d] = true; 
-
-		cout << s << " inserted" << endl; 
-	} 
-} 
-
-// Driver Code 
-int main() 
-{ 
-	bool bitarray[100] = { false }; 
-	int arrSize = 100; 
-	string sarray[33] 
-		= { "abound", "abounds",	 "abundance", 
-			"abundant", "accessible", "bloom", 
-			"blossom", "bolster",	 "bonny", 
-			"bonus", "bonuses",	 "coherent", 
-			"cohesive", "colorful",	 "comely", 
-			"comfort", "gems",		 "generosity", 
-			"generous", "generously", "genial", 
-			"bluff", "cheater",	 "hate", 
-			"war",	 "humanity",	 "racism", 
-			"hurt",	 "nuke",		 "gloomy", 
-			"facebook", "geeksforgeeks", "twitter" }; 
-	for (int i = 0; i < 33; i++) { 
-		insert(bitarray, arrSize, sarray[i]); 
-	} 
-	return 0; 
+    return hash % bitarray.size();
 }
+
+// murmur3 hash
+uint64_t BloomFilter::hash_2(KEY k) const {
+
+    int32_t key = k;
+    uint32_t seed = 42; //arbiturary seed 
+    uint32_t hash = MurmurHash3_x86_32(&key, sizeof(key), seed);
+
+    return hash % bitarray.size();
+}
+
+// xxhash hash
+uint64_t BloomFilter::hash_3(KEY k) const {
+
+    int32_t key = k;
+    uint32_t seed = 42; //arbiturary seed 
+    uint32_t result2 = XXHash32::hash(&key, sizeof(key), seed);
+
+    return 0;
+}
+
+void BloomFilter::set(KEY key) {
+    bitarray.set(hash_1(key));
+    bitarray.set(hash_2(key));
+    bitarray.set(hash_3(key));
+}
+
+bool BloomFilter::is_set(KEY key) const {
+    return (bitarray.test(hash_1(key))
+         && bitarray.test(hash_2(key))
+         && bitarray.test(hash_3(key)));
+}
+
+// murmurhash3 modified from https://github.com/aappleby/smhasher/blob/master/src/MurmurHash3.cpp
+
+uint32_t MurmurHash3_x86_32(const void* key, int len, uint32_t seed) {
+    const uint8_t* data = (const uint8_t*)key;
+    const int nblocks = len / 4;
+    uint32_t h1 = seed;
+    const uint32_t c1 = 0xcc9e2d51;
+    const uint32_t c2 = 0x1b873593;
+
+    // body
+    const uint32_t* blocks = (const uint32_t*)(data + nblocks * 4);
+    for (int i = -nblocks; i; i++) {
+        uint32_t k1 = blocks[i];
+
+        k1 *= c1;
+        k1 = (k1 << 15) | (k1 >> (32 - 15));
+        k1 *= c2;
+
+        h1 ^= k1;
+        h1 = (h1 << 13) | (h1 >> (32 - 13));
+        h1 = h1 * 5 + 0xe6546b64;
+    }
+
+    // tail
+    const uint8_t* tail = (const uint8_t*)(data + nblocks * 4);
+    uint32_t k1 = 0;
+
+    switch (len & 3) {
+        case 3:
+            k1 ^= tail[2] << 16;
+        case 2:
+            k1 ^= tail[1] << 8;
+        case 1:
+            k1 ^= tail[0];
+            k1 *= c1;
+            k1 = (k1 << 15) | (k1 >> (32 - 15));
+            k1 *= c2;
+            h1 ^= k1;
+    }
+
+    // finalization
+    h1 ^= len;
+
+    // fmix
+    h1 ^= h1 >> 16;
+    h1 *= 0x85ebca6b;
+    h1 ^= h1 >> 13;
+    h1 *= 0xc2b2ae35;
+    h1 ^= h1 >> 16;
+
+    return h1;
+};
--- bloom.h
+++ bloom.h
@@ -1,16 +1,22 @@
-// This file defines the functions used by a bloom filter in the LSM-Tree
-#ifndef bloom.h
-#define bloom.h
+#include <boost/dynamic_bitset.hpp>
+#include <xxhash32.h>
+typedef int32_t KEY;
 
-#include <stdio.h>
-#include <cstdint>
-#include <vector>
-#include <array>
-#include <math.h>
+class BloomFilter {
+    // the underlying representation of this should be a bit array. 
+   boost::dynamic_bitset<> bitarray;
+   uint64_t hash_1(KEY) const;
+   uint64_t hash_2(KEY) const;
+   uint64_t hash_3(KEY) const;
 
+public:
+    // constructor
+    BloomFilter(long length) : bitarray(length) {}
 
+    // set bit in bitarray
+    void set(KEY);
 
+    // check bit in bitarray
+    bool is_set(KEY) const;
 
-
-
-#endif
+};
