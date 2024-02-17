847608be6e722771d61434df10d75faa06c56fab
Hongkai Wang <hongkaiwang@hsph.harvard.edu>
2024-02-17 17:13:00 -0500

    cleaned up bloom filter implementation

--- bloom.h
+++ bloom.h
@@ -1,16 +1,17 @@
 #include <boost/dynamic_bitset.hpp>
-#include "xxhash32.h"
+#include "lib/xxhash32.h"
+#include "lib/murmur3.h"
+
 typedef int32_t KEY;
 
 class BloomFilter {
     // the underlying representation of this should be a bit array. 
     boost::dynamic_bitset<> bitarray;
 
-    // hash function for bloom filter.
+    // hash functions for bloom filter.
     uint32_t hash_1(KEY) const;
     uint32_t hash_2(KEY) const;
     uint32_t hash_3(KEY) const;
-    uint32_t MurmurHash3_x86_32(const void *key, int len, uint32_t seed) const;
 
 public:
     // constructor
